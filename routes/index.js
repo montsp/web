63bea01e9204df5e4c6989dfa2090cbca921cd05
montsp <montsp.me@gmail.com>
2024-10-12 18:40:46 +0200

    1

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var axios = require('axios');
+
+
+
+
+router.get('/', async (req, res, next) => {
+
+  try {
+    const response = await axios.get('https://montsp.github.io/data/articles.json'); // 元のプロジェクトのエンドポイントを指定
+    let data = {
+      articles: response.data,
+    };
+    res.render('index', data);
+  } catch (error) {
+    res.status(500).send('Error fetching data');
+  }
+});
+
+router.get('/about', (req, res, next) => {
+  res.render('about');
+});
+
+//http://localhost:3000/?param=json
+
+module.exports = router;
